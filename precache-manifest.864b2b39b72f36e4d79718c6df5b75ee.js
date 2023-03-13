55f5f15485039af6aa1bb8affbec20115147e5cf
Michael Clancy <mike@wewritecode.com>
2023-03-13 11:01:13 -0500

    Updates

--- precache-manifest.864b2b39b72f36e4d79718c6df5b75ee.js
+++ precache-manifest.864b2b39b72f36e4d79718c6df5b75ee.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58206d694c41c2c3dc2c70accfd32133",
+    "revision": "27caa090529c6205560847abdb0636bd",
     "url": "/b2c-scoring-app/index.html"
   },
   {
-    "revision": "2634f96d4ce1951c48fe",
-    "url": "/b2c-scoring-app/static/css/main.fe55e097.chunk.css"
+    "revision": "a046f94d72d0d1aab23b",
+    "url": "/b2c-scoring-app/static/css/main.11c0bf0b.chunk.css"
   },
   {
     "revision": "29568dc86e74e291dede",
     "url": "/b2c-scoring-app/static/js/2.a4e2fc17.chunk.js"
   },
   {
-    "revision": "2634f96d4ce1951c48fe",
+    "revision": "a046f94d72d0d1aab23b",
     "url": "/b2c-scoring-app/static/js/main.5f833428.chunk.js"
   },
   {
